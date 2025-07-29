61663aa159de13cbeffebb28468bbc1d53f96cee
Ethan1sp <ethan.noshie@dcmail.ca>
2025-07-29 18:14:56 -0400

    Update

--- Assignment 4/Image Gallery/main.js
+++ Assignment 4/Image Gallery/main.js
@@ -7,35 +7,35 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 const alts = {
-  'pic1.jpg' : 'Closeup of a human eye',
-  'pic2.jpg' : 'Rock that looks like a wave',
-  'pic3.jpg' : 'Purple and white pansies',
-  'pic4.jpg' : 'Section of wall from a pharaoh\'s tomb',
-  'pic5.jpg' : 'Large moth on a leaf'
-}
+  'pic1.jpg': 'Closeup of a human eye',
+  'pic2.jpg': 'Rock that looks like a wave',
+  'pic3.jpg': 'Purple and white pansies',
+  'pic4.jpg': "Section of wall from a pharaoh's tomb",
+  'pic5.jpg': 'Large moth on a leaf'
+};
 
-/* Looping through images */
+/* Looping through images and creating thumbnails */
 for (const image of images) {
   const newImage = document.createElement('img');
-  newImage.setAttribute('src', `images/${image}`);
-  newImage.setAttribute('alt', alts[image]);
+  newImage.src = `images/${image}`;
+  newImage.alt = alts[image];
   thumbBar.appendChild(newImage);
-  newImage.addEventListener('click', e => {
-    displayedImage.src = e.target.src;
-    displayedImage.alt = e.target.alt;
+
+  newImage.addEventListener('click', () => {
+    displayedImage.src = newImage.src;
+    displayedImage.alt = newImage.alt;
   });
 }
 
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener('click', () => {
-  const btnClass = btn.getAttribute('class');
-  if (btnClass === 'dark') {
-    btn.setAttribute('class', 'light');
+  if (btn.classList.contains('dark')) {
+    btn.classList.replace('dark', 'light');
     btn.textContent = 'Lighten';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
   } else {
-    btn.setAttribute('class', 'dark');
+    btn.classList.replace('light', 'dark');
     btn.textContent = 'Darken';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
   }
 });
